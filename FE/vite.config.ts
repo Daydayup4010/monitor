2335e55e767842fa8dc1f638ece91a666b89cc22
zhengwei.zeng@ui.com <zhengwei.zeng@ui.com>
2025-09-11 15:12:29 +0800

    update nginx config

--- FE/vite.config.ts
+++ FE/vite.config.ts
@@ -12,11 +12,13 @@ export default defineConfig({
   },
   server: {
     port: 3000,
+    host: '0.0.0.0', // 允许外部访问
     proxy: {
       '/api': {
         target: 'http://localhost:8080',
         changeOrigin: true,
       }
     }
-  }
+  },
+  base: '/csgo/' // 设置基础路径，匹配 nginx 的 location
 })
