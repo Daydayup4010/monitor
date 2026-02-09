941522ad910e4bca7ad9cbe43bd3edfb4ed717b0
zhengwei.zeng@ui.com <zhengwei.zeng@ui.com>
2026-02-09 22:58:21 +0800

    1.add get steam id

--- models/steam.go
+++ models/steam.go
@@ -64,7 +64,7 @@ func BatchUpdateSteamGoods(steam []*Steam) {
 // GetSteamsWithoutItemNameId 获取所有没有 item_nameid 的商品
 func GetSteamsWithoutItemNameId() ([]Steam, error) {
 	var steams []Steam
-	err := config.DB.Where("id = '' OR id IS NULL").Find(&steams).Error
+	err := config.DB.Where("(id = '' OR id IS NULL) AND sell_price < 500").Find(&steams).Error
 	return steams, err
 }
 
